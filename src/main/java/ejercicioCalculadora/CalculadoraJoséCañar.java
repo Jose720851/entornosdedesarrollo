2138c4a43a37e0fbf9ced029bdfe7d56a0dc848a
jose <jose.canar@educa.madrid.org>
2026-01-31 14:21:19 +0100

    Commit inicial del proyecto Maven

---
+++ src/main/java/ejercicioCalculadora/CalculadoraJoséCañar.java
@@ -0,0 +1,29 @@
+package ejercicioCalculadora;
+public class CalculadoraJoséCañar {
+    private final int num1;
+    private final int num2;
+    public CalculadoraJoséCañar(int a, int b) {
+        num1 = a;
+        num2 = b;
+    }
+    public int suma() {
+        return num1 + num2;
+    }
+    public int resta() {
+        return num1 - num2;
+    }
+    public int multiplica() {
+        return num1 * num2;
+    }
+    public int divide() {
+        return num1 / num2;
+    }
+    public int resta2() {
+        return num1 >= num2 ? num1 - num2 : num2 - num1;
+    }
+    public int divide2() {
+        if (num2 == 0) return 0;
+        return num1 / num2;
+    }
+
+}

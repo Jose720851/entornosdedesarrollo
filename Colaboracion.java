1ab75087e129fd1f80f5f68f7c73f27e102eea11
jose <jose.canar@educa.madrid.org>
2026-01-31 16:57:02 +0000

    añadido mensaje desde Ubuntu

---
+++ Colaboracion.java
@@ -0,0 +1,6 @@
+public class Colaboracion {
+	public static void main(Strings[] args){
+		System.out.println("Versión inicial de los proyectos");
+		System.out.println("mensaje desde Ubuntu.");
+		}
+}

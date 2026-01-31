dd281bab7338baaf3d0d21898a01582caeead859
jose <jose.canar@educa.madrid.org>
2026-01-31 17:41:51 +0100

    Commit inicial del proyecto Maven

---
+++ src/test/java/ejercicioCalculadora/colaboracion.java
@@ -0,0 +1,6 @@
+package ejercicioCalculadora;
+public class colaboracion {
+        public static void main(String[] args) {
+            System.out.println("Versión inicial del proyecto.");
+        }
+    }
